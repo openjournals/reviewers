4737abdf554c582f7e8af0a35fd134f46420daed
Juanjo Bazán <jjbazan@gmail.com>
2022-08-24 11:21:25 +0200

    Change scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['REVIEWERS_GH_CLIENT_ID'], ENV['REVIEWERS_GH_CLIENT_SECRET'], scope: "public_repo"
+  provider :github, ENV['REVIEWERS_GH_CLIENT_ID'], ENV['REVIEWERS_GH_CLIENT_SECRET'], scope: "read:user"
   provider :orcid, ENV['REVIEWERS_ORCID_CLIENT_ID'], ENV['REVIEWERS_ORCID_CLIENT_SECRET']
 end
