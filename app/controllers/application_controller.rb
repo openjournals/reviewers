22a051aa45cd82df9796b41bc77656107af636c3
Juanjo Bazán <jjbazan@gmail.com>
2022-10-20 11:51:53 +0200

    Add editor filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def require_user
     unless current_user
       flash[:error] = "Please login first"
       redirect_back fallback_location: :root
-      false # throw :abort
+      false
+    end
+  end
+
+  def require_editor
+    unless current_editor
+      redirect_to :root
+      false
     end
   end
 
@@ -14,4 +22,17 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def current_editor
+    if current_user && current_user.editor?
+      @current_editor = current_user
+    else
+      @current_editor = nil
+    end
+  end
+  helper_method :current_editor
+
+  def record_not_found
+    render plain: "404 Not Found", status: 404
+  end
 end
