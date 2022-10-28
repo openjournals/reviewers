aa9a9400ad35c6e9503098be5df6889f480364af
Juanjo Bazán <jjbazan@gmail.com>
2022-10-28 13:54:27 +0200

    Update languages

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,10 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-LANGUAGES_LIST = %w(Ada Bash C/C++/C# Clojure CUDA Elixir Erlang Fortran Go Haskell
-                    Java Javascript Julia LaTeX Lisp OpenCL Pascal Perl PHP Python
-                    Mathematica Matlab R Ruby Rust Scala SQL Swift)
+LANGUAGES_LIST = %w(Ada Bash/Shell Basic C/C++/C# Clojure CUDA Elixir Erlang Fortran F# Go Haskell
+                    Java Javascript Julia Kotlin LaTeX Lisp Lua OCaml Octave OpenCL Pascal Perl PHP Python
+                    Mathematica Matlab R Ruby Rust Scala SQL Swift Typescript)
+
 LANGUAGES_LIST.each do |lang|
   Language.find_or_create_by(name: lang)
 end
