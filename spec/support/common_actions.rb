39b581f4b956e2367d2cb9c97bce497fe3fb93bc
Juanjo Bazán <jjbazan@gmail.com>
2022-11-16 11:38:38 +0100

    Add CommonActions module

---
+++ spec/support/common_actions.rb
@@ -0,0 +1,16 @@
+module CommonActions
+  def login_as(user)
+    allow_any_instance_of(ApplicationController).
+      to receive(:current_user).and_return(user)
+  end
+
+  def logout
+    allow_any_instance_of(ApplicationController).
+      to receive(:current_user).and_return(nil)
+  end
+end
+
+RSpec.configure do |config|
+  config.include CommonActions
+end
+
