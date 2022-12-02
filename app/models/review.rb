4433072d55c29b8e9b21c5af9b2b56dd8cc4d7fc
Juanjo Bazán <jjbazan@gmail.com>
2022-12-02 15:14:13 +0100

    Add counter cache for user's reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, counter_cache: true
 
   validates_presence_of :user_id
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_02_133937) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_141132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -97,6 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_133937) do
     t.integer "feedback_score", default: 0
     t.integer "feedback_score_last_3", default: 0
     t.integer "feedback_score_last_year", default: 0
+    t.integer "reviews_count", default: 0
     t.index ["complete_name"], name: "index_users_on_complete_name"
     t.index ["github"], name: "index_users_on_github"
     t.index ["orcid"], name: "index_users_on_orcid"
