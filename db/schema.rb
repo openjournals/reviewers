fec5e0e5fd5f1d1936d8d830cec6db5846523863
Juanjo Bazán <jjbazan@gmail.com>
2022-12-02 14:41:12 +0100

    Add date to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_104511) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_133937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_104511) do
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
